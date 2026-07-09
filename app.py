ecea962263a85c2f04f8559529e2990830c94ad0
jadebregeon <jade.bregeon@gmail.com>
2026-07-09 17:09:59 +0200

    changement_df

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ if st.session_state["cp"] is not None:
 if st.session_state["lat"] is not None:
     if st.session_state['liste_ville']:
                 
-        df = pd.read_csv("data/f.csv")
+        df = pd.read_csv("data/df.csv")
         
 
         # DATA FILTRE PAR LES VILLES
